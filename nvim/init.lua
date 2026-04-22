f89884eb81c7c9eeb2c6589970a1a16c7999d0a0
sicili <li.conner622@gmail.com>
2026-04-22 14:04:45 -0400

    nvim config restructure

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,25 @@
-require("sicili")
+----------------------------------------------
+-------------- NEOVIM OPTIONS ----------------
+----------------------------------------------
+
+-- Disable Netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Sets Keymap Leader
+vim.g.mapleader = ' '
+
+------------ Lazy.nvim Startup ---------------
+require("core.lazy")
+
+----------- Keymaps On Startup ---------------
+require("core.keymaps")
+
+----------- Settings On Startup --------------
+require("core.options")
+
+------- Enable Treesitter Highlighting -------
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'lua', 'c', 'java', 'javascript', 'jsx', 'typescript', 'glsl', },
+  callback = function() vim.treesitter.start() end,
+})
